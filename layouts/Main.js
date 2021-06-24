a1e152174f032bb6dcd7b15172cac5d22bc46bf4
Dani Domínguez <dani.d.zamorano@gmail.com>
2021-06-24 13:34:53 +0200

    layout improvements

--- layouts/Main.js
+++ layouts/Main.js
@@ -2,7 +2,7 @@ import { Flex, VStack } from '@chakra-ui/layout';
 
 const MainLayout = ({ children }) => (
   <Flex justify="center" alignItems="center" h="100vh">
-    <VStack w={['100vw', 'md']} h={['100vh', '80vh']} bg="white" boxShadow="md" spacing="0" position="relative">
+    <VStack w={['100vw', 'md']} h={['100vh', '80vh']} bg="white" boxShadow="md" position="relative">
       {children}
     </VStack>
   </Flex>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
-import { Box } from '@chakra-ui/layout';
 import Header from '../components/Header/Header';
 
 export default function Home() {
-  return (
-    <Box overflowY="auto" w="100%" flexGrow={1}>
-      <Header />
-    </Box>
-  );
+  return <Header />;
 }
